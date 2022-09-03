e8e3cd63189e8735bcaf9c0330041cbc964e89b5
Konstantin Vavilov <gravekon111@yandex.ru>
2022-09-03 20:30:03 +0500

    Исправить замечания ревьюера

--- pages/script.js
+++ pages/script.js
@@ -1,10 +1,10 @@
-const mainForm = document.querySelector('.form__header');
+const mainForm = document.querySelector('.header__form');
 const fieldForm = mainForm.querySelector('.contact-form__input');
 const btnForm = mainForm.querySelector('.contact-form__btn');
 const spanFormBtn = mainForm.querySelector('.contact-form__btn-text');
 const btnImage = mainForm.querySelector('.contact-form__image');
 
-const footerForm = document.querySelector('.form__footer');
+const footerForm = document.querySelector('.footer__form');
 const fieldFooterForm = footerForm.querySelector('.contact-form__input');
 const btnFooterForm = footerForm.querySelector('.contact-form__btn');
 const spanFormFooterBtn = footerForm.querySelector('.contact-form__btn-text');
@@ -13,11 +13,11 @@ const btnFooterImage = footerForm.querySelector('.contact-form__image');
 function changeBtn(evt) {
   evt.preventDefault();
 
-  if (evt.target.classList.contains('form__header') && fieldForm.value) {
+  if (evt.target.classList.contains('header__form') && fieldForm.value) {
     spanFormBtn.textContent = 'Круто, спасибо за доверие!';
     btnImage.classList.add('contact-form__image_nonactive');
   }
-  else if (evt.target.classList.contains('form__footer') && fieldFooterForm.value) {
+  else if (evt.target.classList.contains('footer__form') && fieldFooterForm.value) {
     spanFormFooterBtn.textContent = 'Круто, спасибо за доверие!';
     btnFooterImage.classList.add('contact-form__image_nonactive');
   }
